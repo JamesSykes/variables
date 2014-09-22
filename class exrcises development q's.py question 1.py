26c172ee67b2e22cf9b68842a106043544cdb314
James Sykes <40792@longroad.ac.uk>
2014-09-22 10:01:20 +0100

    class exercises developmnt q's

---
+++ class exrcises development q's.py question 1.py
@@ -0,0 +1,9 @@
+#James Sykes
+#22-09-2014
+#Development quesions 2.0
+
+print("please enter a number:")
+print("please enter a different number:")
+number1=int(input("please enter a number:"))
+number2=int(input("please enter a different  numbe:"))
+answer=input(number1%number2)
